b97a72082faa9dd1453267d578914e7572823091
Nuratkumar <22cse731.tarunkumarpalka@giet.edu>
2025-07-27 22:48:08 +0530

    changed bro

--- iot1.py
+++ iot1.py
@@ -1,39 +1,37 @@
-import time,random,sqlite3
+import time, random, sqlite3
 
-LIMIT=35.0
+LIMIT = 35.0
 
-conn=sqlite3.connect("teju.db")
-cursor=conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS readings(
-               id INTEGER PRIMARY KEY AUTOINCREMENT,
-               times REAL,
-               temp REAL,
-               hum REAL,
-               )''')
+conn = sqlite3.connect("teju.db")
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS readings (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        times REAL,
+        temp REAL,
+        hum REAL
+    )
+''')
 conn.commit()
 
 def read_sensor():
-    temp=round(random.uniform(20,45),2)
-    hum=round(random.uniform(30,90),2)
-    return temp,hum
+    temp = round(random.uniform(20, 45), 2)
+    hum = round(random.uniform(30, 90), 2)
+    return temp, hum
 
 while True:
-    temp,hum=read_sensor()
-    times=time.time()
+    temp, hum = read_sensor()
+    times = time.time()
 
     cursor.execute('''
-                    INSERT INTO readings(times,temp,hum)
-                   values(?,?,?)''',(times,temp,hum))
+        INSERT INTO readings(times, temp, hum)
+        VALUES (?, ?, ?)
+    ''', (times, temp, hum))
     conn.commit()
 
-    print(f"Logged: tempature={temp}c,humidity={hum}%")
+    print(f"Logged: temperature={temp}°C, humidity={hum}%")
 
-    if temp>LIMIT:
-        print("alert HIGH TEMP!!!!")
+    if temp > LIMIT:
+        print("⚠️ ALERT: HIGH TEMPERATURE!")
 
     time.sleep(2)
-
-
-
-# 
-# ranjit2798
